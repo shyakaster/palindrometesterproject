a8c0c283acf914e96cef8a664325a5378c240c34
alex shyaka <shyakaster@gmail.com>
2018-09-25 12:08:23 +0300

    Add functionality to check for empty phrases

--- bundle.js
+++ bundle.js
@@ -1,4 +1,17 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("shyakaster-palindrome");
+
+let string = prompt("Please enter a string for palindrome testing");
+
+let phrase = new Phrase(string);
+
+if(phrase.palindrome()){
+    alert(`"${phrase.content}" is a palindrome!`);
+}else{
+    alert(`"${phrase.content}" is not a palindrome!`)
+}
+
+},{"shyakaster-palindrome":2}],2:[function(require,module,exports){
 
 module.exports = Phrase;
 //Modifies a String native object
@@ -25,17 +38,4 @@ function Phrase(content){
 
 
 
-},{}],2:[function(require,module,exports){
-let Phrase = require("shyakaster-palindrome");
-
-let string = prompt("Please enter a string for palindrome testing");
-
-let phrase = new Phrase(string);
-
-if(phrase.palindrome()){
-    alert(`"${phrase.content}" is a palindrome!`);
-}else{
-    alert(`"${phrase.content}" is not a palindrome!`)
-}
-
-},{"shyakaster-palindrome":1}]},{},[2]);
+},{}]},{},[1]);
